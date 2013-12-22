ae5b4cd07152e91bea01bec723e65b08862ebfb5
Fons <lord.fontana@gmail.com>
2013-12-22 13:10:46 +0100

    problema visibilita import e creato DTO aggiunto commento sul problema
    Signed-off-by: Fons <lord.fontana@gmail.com>

---
+++ TravelDream/TravelDreamEJBClient/ejbModule/com/traveldream/autenticazione/ejb/dto/UserDTO.java
@@ -0,0 +1,66 @@
+package com.traveldream.autenticazione.ejb.dto;
+
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class UserDTO {
+	@NotEmpty 
+	private String username;
+
+	@Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            message="invalid email")
+	private String email;
+	
+	@NotEmpty
+    private String firstName;
+	
+	@NotEmpty
+    private String lastName;
+	
+	@NotEmpty
+    private String password;
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+     
+    public String getFirstName() {
+        return firstName;
+    }
+     
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+  
+    public String getLastName() {
+        return lastName;
+    }
+  
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+  
+    public String getEmail() {
+        return email;
+    } 
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+    public String getPassword() {
+        return password;
+    }
+     
+    public void setPassword(String password) {
+        this.password = password;
+    }
+     
+
+}
---
+++ TravelDream/TravelDreamWeb/src/com/traveldream/autenticazione/web/RegistrationBean.java
@@ -0,0 +1,17 @@
+package com.traveldream.autenticazione.web;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.ejb.EJB;
+import javax.faces.bean.RequestScoped;
+
+
+
+
+
+@ManagedBean(name="registrationBean")
+@RequestScoped
+public class RegistrationBean {
+	
+	
+}
