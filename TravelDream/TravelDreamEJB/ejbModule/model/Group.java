417167ad8881c098aed1b3303556c7d4ff480536
Fons <lord.fontana@gmail.com>
2013-12-24 00:49:41 +0100

    aggiunte librerie apache.common
    Signed-off-by: Fons <lord.fontana@gmail.com>

---
+++ TravelDream/TravelDreamEJB/ejbModule/model/Group.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Group {
+	AMMINISTRATORE, UTENTE,IMPIEGATO;
+	
+	public static final String _AMMINISTRATORE = "AMMINISTRATORE";
+	public static final String _UTENTE = "UTENTE";
+	public static final String _IMPIEGATO = "IMPIEGATO";
+
+}
