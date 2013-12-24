8730bfea6a6f7d5abf105a552a0ded43a4ef0184
fabio <fbi.gritti@gmail.com>
2013-12-24 13:26:20 +0100

    sistemato userDTO

--- TravelDream/TravelDreamEJBClient/src/com/traveldream/autenticazione/ejb/UserDTO.java
+++ TravelDream/TravelDreamEJBClient/src/com/traveldream/autenticazione/ejb/UserDTO.java
@@ -1,7 +1,8 @@
 package com.traveldream.autenticazione.ejb;
 
-import java.sql.Date;
+import java.util.Date;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -23,7 +24,7 @@ public class UserDTO {
 	@NotEmpty
     private String password;
 	
-	@NotEmpty
+	@NotNull
 	private Date data;
 	
 
