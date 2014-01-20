7cce0df46f8b3fbb786955e895b383fc47a1363c
fabio <fbi.gritti@gmail.com>
2014-01-20 22:13:27 +0100

    commit pre-merge
    Signed-off-by: fabio <fbi.gritti@gmail.com>

--- TravelDream/TravelDreamEJBClient/src/com/traveldream/gestioneprenotazione/ejb/BookManagerBeanLocal.java
+++ TravelDream/TravelDreamEJBClient/src/com/traveldream/gestioneprenotazione/ejb/BookManagerBeanLocal.java
@@ -3,7 +3,8 @@ package com.traveldream.gestioneprenotazione.ejb;
 public interface BookManagerBeanLocal {
 
 	
-	
+	void saveViaggio(ViaggioDTO v);
+	int cercaViaggio(ViaggioDTO viaggiodto);
 	
 	
 }
--- TravelDream/TravelDreamWeb/src/com/traveldream/viaggio/web/ViaggioBean.java
+++ TravelDream/TravelDreamWeb/src/com/traveldream/viaggio/web/ViaggioBean.java
@@ -1,9 +1,11 @@
 package com.traveldream.viaggio.web;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import com.traveldream.gestionecomponente.ejb.EscursioneDTO;
@@ -17,28 +19,24 @@ import com.traveldream.gestionepack.ejb.PackManagerBeanLocal;
 import com.traveldream.gestionepack.web.EscDataModel;
 import com.traveldream.gestionepack.web.HotelDataModel;
 import com.traveldream.gestionepack.web.VoloDataModel;
+import com.traveldream.gestioneprenotazione.ejb.BookManagerBeanLocal;
 import com.traveldream.gestioneprenotazione.ejb.ViaggioDTO;
 
 
 @ManagedBean(name="ViaggioBean") 
-@ViewScoped
+@SessionScoped
 public class ViaggioBean {
 	
 	@EJB
 	private PackManagerBeanLocal PMB;
+    
+	@EJB
+	private BookManagerBeanLocal BMB; 
 	
-	public HotelDTO getSelectedHotels() {
-		return selectedHotels;
-	}
-
-	public EscursioneDTO getSelectedEsc() {
-		return selectedEsc;
-	}
-
 	private HotelDTO selectedHotels;
 	private VoloDTO selectedVolo_a;
 	private VoloDTO selectedVolo_r;
-	private EscursioneDTO selectedEsc;
+	private ArrayList <EscursioneDTO> selectedEsc;
 	
 	
 	private HotelDataModel hotelModels;
@@ -53,11 +51,21 @@ public class ViaggioBean {
 	private PacchettoDTO packet;
 
     private ViaggioDTO viaggio;
+    private Date data_inizio;
+    private Date data_fine;
+    
+    private int n_partecipanti;
+    private int quotacomplessiva;
+    private int quotapp;
+   
+
+	private String password1;
 	
 	public ViaggioBean() {
 		
 		 packet = new PacchettoDTO();
-		
+		 viaggio = new ViaggioDTO();
+		 selectedEsc = new ArrayList <EscursioneDTO>();
 	}
 
 	public HotelDataModel getHotelModels() {
@@ -115,7 +123,16 @@ public class ViaggioBean {
 		this.selectedHotels = selectedHotels;
 	}
 
-	public void setSelectedEsc(EscursioneDTO selectedEsc) {
+	public HotelDTO getSelectedHotels() {
+		return selectedHotels;
+	}
+
+
+	public ArrayList<EscursioneDTO> getSelectedEsc() {
+		return selectedEsc;
+	}
+
+	public void setSelectedEsc(ArrayList<EscursioneDTO> selectedEsc) {
 		this.selectedEsc = selectedEsc;
 	}
 
@@ -161,14 +178,62 @@ public class ViaggioBean {
 	
 	public String acquista_paga()
 	{
-		if(selectedHotels == null || selectedVolo_a == null || selectedVolo_r == null)
+		int id;
+		if(selectedHotels==null || selectedVolo_a == null || selectedVolo_r == null)
 		  {
-			return null;
+			return "userhome.xhtml?faces-redirect=true";
 		  }
 		
-		//creazione viaggio bla bla bla...
+		viaggio.setHotel(selectedHotels);
+		viaggio.setVolo_andata(selectedVolo_a);
+		viaggio.setVolo_ritorno(selectedVolo_r);		  
+		viaggio.setLista_escursioni(selectedEsc);
+		viaggio.setData_inizio(this.data_inizio);
+		viaggio.setData_fine(this.data_fine);
 		
-		return "pagamento.xhtml?faces-redirect=true"; 	
+		id = BMB.cercaViaggio(viaggio); // cerco se qualcuno ha già creato un viaggio del genere per non mettere doppioni nel db
+		
+		if(id == -1) // se non era presente il viaggio che stavo creando allora lo salvo
+	       { 
+			 BMB.saveViaggio(viaggio);
+	         //id = BMB.cercaViaggio(viaggio);
+	       }
+	   
+		return "pagamento.xhtml?faces-redirect=true"; 	//&id=" +id
+	}
+	
+	public void calcoloquota()
+	{
+      int cg = viaggio.getHotel().getCosto_giornaliero();
+      
+      int duration = (int) (( viaggio.getData_fine().getTime() - viaggio.getData_inizio().getTime() ) / (1000 * 60 * 60 * 24));
+	
+      int hotel_stay = cg * duration;
+      
+      int costi_voli = viaggio.getVolo_andata().getCosto() + viaggio.getVolo_ritorno().getCosto();
+      
+      int costi_escursioni=0;
+      
+      for(EscursioneDTO edto: viaggio.getLista_escursioni())
+         {
+    	  costi_escursioni+=edto.getCosto();
+         }
+     this.setQuotapp(cg+costi_voli+costi_escursioni+hotel_stay);
+	 this.setQuotacomplessiva(this.getQuotapp());
+	}
+	
+	public void updatePrice()
+	{
+		this.setQuotacomplessiva(this.getN_partecipanti() * this.getQuotapp());
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXquotacomplessiva" + this.getQuotacomplessiva());
+	}
+	
+	public void creaPrenotazione()
+	{
+		
+		
+		
+		
 	}
 	
 	
@@ -180,6 +245,7 @@ public class ViaggioBean {
 		  {
 			return null;
 		  }
+		
 		
 		
 		//creazione entita gift
@@ -214,4 +280,78 @@ public class ViaggioBean {
 	public void setViaggio(ViaggioDTO viaggio) {
 		this.viaggio = viaggio;
 	}
+
+	public int getN_partecipanti() {
+		return n_partecipanti;
+	}
+
+	public void setN_partecipanti(int n_partecipanti) {
+		this.n_partecipanti = n_partecipanti;
+	}
+
+	public Date getData_inizio() {
+		return data_inizio;
+	}
+
+	public void setData_inizio(Date data_inizio) {
+		this.data_inizio = data_inizio;
+	}
+
+	public Date getData_fine() {
+		return data_fine;
+	}
+
+	public void setData_fine(Date data_fine) {
+		this.data_fine = data_fine;
+	}
+
+	public int getQuotacomplessiva() {
+		return quotacomplessiva;
+	}
+
+	public void setQuotacomplessiva(int quotacomplessiva) {
+		this.quotacomplessiva = quotacomplessiva;
+	}
+
+	public String getPassword1() {
+		return password1;
+	}
+
+	public void setPassword1(String password1) {
+		this.password1 = password1;
+	}
+
+	 public int getQuotapp() {
+			return quotapp;
+		}
+
+		public void setQuotapp(int quotapp) {
+			this.quotapp = quotapp;
+		}
+	
+	/*
+	 * dovrebbe servire per filtrare i risultati in base alle date di inzio e fine del viaggio ( TODO )
+	public void filterComponents(){
+		
+		 filteredEscursiones = PMB.getListaEscursioniCompatibili(packet.getDestinazione(), this.data_inizio, this.data_fine);
+		 filteredHotels = PMB.getListaHotelCompatibili(packet.getDestinazione(), this.data_inizio, this.data_fine);
+		 
+		 filteredVolos = PMB.getListaVoliCompatibili(packet.getDestinazione(), this.data_inizio, this.data_fine);
+		
+		 ArrayList <VoloDTO> filteredVolo_a = new ArrayList <VoloDTO>();
+		 ArrayList <VoloDTO> filteredVolo_r = new ArrayList <VoloDTO> ();
+		 
+		 for(VoloDTO v: filteredVolos)
+		    {
+			 if(v.getLuogo_partenza().equals(this.packet.getDestinazione()))
+				 filteredVolo_a.add(v);
+			 else
+				 
+		    }
+		 
+		 setHotelModels(new HotelDataModel(filteredHotels));	
+		 setVoloModels(new VoloDataModel(filteredVolos));
+		 setEscModels(new EscDataModel(filteredEscursiones));
+	}
+	*/
 }
