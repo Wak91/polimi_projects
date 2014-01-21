9d26cc3291b682b34b8653351238c8ef62526487
Fons <lord.fontana@gmail.com>
2014-01-21 22:04:26 +0100

    sistemato addtogift list
    Signed-off-by: Fons <lord.fontana@gmail.com>

--- TravelDream/TravelDreamEJB/ejbModule/com/traveldream/condivisione/ejb/GiftListManagerBean.java
+++ TravelDream/TravelDreamEJB/ejbModule/com/traveldream/condivisione/ejb/GiftListManagerBean.java
@@ -1,11 +1,30 @@
 package com.traveldream.condivisione.ejb;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.EscursionePagata;
+
+import com.sun.xml.rpc.processor.modeler.j2ee.xml.emptyType;
 
 @Stateless
 public class GiftListManagerBean implements GiftListManagerBeanLocal {
 	
+	@PersistenceContext
+    private EntityManager em;
+	
+	@Resource
+	private EJBContext context;
+	
+	
 	public void addToGiftList(GiftListDTO giftListDTO){
+		//crea viaggio
+		for (EscursionePagataDTO escursionePagataDTO : giftListDTO.getEscursionePagata()) {
+			EscursionePagata escursionePagata = em.find(EscursionePagata.class,escursionePagataDTO.getId() );
+		}
 		
 		
 	}
