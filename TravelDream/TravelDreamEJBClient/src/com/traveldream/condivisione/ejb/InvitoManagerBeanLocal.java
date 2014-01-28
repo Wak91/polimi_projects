32c15967656c2d37ba759e7d4b350c4452c2e0ff
motric <motric@hotmail.it>
2014-01-28 20:56:13 +0100

    aggiunti controllo e eliminazione inviti

--- TravelDream/TravelDreamEJBClient/src/com/traveldream/condivisione/ejb/InvitoManagerBeanLocal.java
+++ TravelDream/TravelDreamEJBClient/src/com/traveldream/condivisione/ejb/InvitoManagerBeanLocal.java
@@ -19,4 +19,6 @@ public interface InvitoManagerBeanLocal {
 
 	InvitoDTO cercaInvitoById(int id);
 
+	void eliminaInvito(int id);
+
 }
--- TravelDream/TravelDreamWeb/src/com/traveldream/condivisione/web/InvitoBean.java
+++ TravelDream/TravelDreamWeb/src/com/traveldream/condivisione/web/InvitoBean.java
@@ -148,7 +148,7 @@ public class InvitoBean {
 		IMB.cambiaStato(invito);
 		System.out.println("stato cambiato");
 		System.out.println(invito.getStatus());
-		return "/homepage.xhtml?faces-redirect=true";
+		return "/utente/userhome.xhtml?faces-redirect=true";
 	}
 
 
@@ -171,10 +171,12 @@ public class InvitoBean {
 		try {
 			InvitoDTO invito_dto = IMB.cercaInvitoById(getId_amico(), getMail());
 			setInvito(invito_dto);
+			if (userMgr.getUserDTO().getEmail() != invito_dto.getUtente().getEmail()){
 			System.out.println(invito_dto.getViaggio().getHotel().getNome());
 			System.out.println(invito_dto.getAmico());
 			System.out.println(invito_dto.getStatus());
 			return "/utente/confermaInvito.xhtml?faces-redirect=true";
+			} else { return "/utente/userhome.xhtml?faces-redirect=true"; }
 		} catch (NullPointerException e){
 			System.out.println("null");
 			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "What we do in life", "Echoes in eternity.");
@@ -197,6 +199,11 @@ public class InvitoBean {
 	        FacesContext.getCurrentInstance().addMessage("null", message);
 			return "/utente/imieiviaggi.xhtml?faces-redirect=true";
 		}	
+	}
+	
+	public String deleteInvito(int id){
+		IMB.eliminaInvito(id);
+		return "/utente/imieiviaggi.xhtml?faces-redirect=true";
 	}
 	
 }
