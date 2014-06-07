cc62672bff9c6697d39f83c2e2e98cbffaa516e0
Sebastiano Mariani <seba0691@gmail.com>
2014-06-07 15:28:22 +0200

    commentato file maìncante
    Signed-off-by: Sebastiano Mariani <seba0691@gmail.com>

---
+++ androBenchmark/obj/local/mips/objs/app_androbenchmark_Matrix/app_androbenchmark_Matrix.o.d
@@ -0,0 +1,76 @@
+obj/local/mips/objs/app_androbenchmark_Matrix/app_androbenchmark_Matrix.o: \
+ jni/app_androbenchmark_Matrix.c jni/app_androbenchmark_Matrix.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/jni.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/cdefs.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/android/api-level.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/stdlib.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/string.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/malloc.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/alloca.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/strings.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/stdint.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/_types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/machine/_types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/_wchar_limits.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/posix_types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/stddef.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/compiler.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/posix_types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/sgidefs.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/types.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/machine/kernel.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/sysmacros.h \
+ /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/memory.h
+
+jni/app_androbenchmark_Matrix.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/jni.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/cdefs.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/android/api-level.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/stdlib.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/string.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/malloc.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/alloca.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/strings.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/stdint.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/_types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/machine/_types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/_wchar_limits.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/posix_types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/stddef.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/compiler.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/posix_types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/sgidefs.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/asm/types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/linux/types.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/machine/kernel.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/sys/sysmacros.h:
+
+/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-mips/usr/include/memory.h:
