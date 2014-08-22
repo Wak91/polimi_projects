1845c90551f56c095c9153ed4624f4f7656ea9ba
fabio <fbi.gritti@gmail.com>
2014-08-22 13:56:36 +0200

    merging
    Signed-off-by: fabio <fbi.gritti@gmail.com>

--- androBenchmark/obj/local/x86/objs/app_androbenchmark_Matrix/app_androbenchmark_Matrix.o.d
+++ androBenchmark/obj/local/x86/objs/app_androbenchmark_Matrix/app_androbenchmark_Matrix.o.d
@@ -1,79 +1,79 @@
 obj/local/x86/objs/app_androbenchmark_Matrix/app_androbenchmark_Matrix.o: \
  jni/app_androbenchmark_Matrix.c jni/app_androbenchmark_Matrix.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/jni.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/api-level.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdlib.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/string.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/malloc.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/alloca.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/strings.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdint.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/_types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/posix_types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/stddef.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/compiler.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/types.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/kernel.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/memory.h \
- /Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/log.h
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/jni.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/api-level.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdlib.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/string.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/malloc.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/alloca.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/strings.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdint.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/_types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/posix_types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/stddef.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/compiler.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/types.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/kernel.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/memory.h \
+ /home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/log.h
 
 jni/app_androbenchmark_Matrix.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/jni.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/jni.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/api-level.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/api-level.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdlib.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdlib.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/string.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/string.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/malloc.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/malloc.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/alloca.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/alloca.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/strings.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/strings.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdint.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/stdint.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/_types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/_types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/posix_types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/posix_types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/stddef.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/stddef.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/compiler.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/compiler.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/asm/types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/types.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/linux/types.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/kernel.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/machine/kernel.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/memory.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/memory.h:
 
-/Users/seba0691/Developer/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/log.h:
+/home/degrigis/Scrivania/android-ndk-r9d/platforms/android-19/arch-x86/usr/include/android/log.h:
