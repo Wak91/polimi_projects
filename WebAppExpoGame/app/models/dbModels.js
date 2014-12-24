adbb42cea482405f9269268b1b1e9fbbe37c6552
Andrea Corna <andrea.corna.ac.91@gmail.com>
2014-12-24 16:56:58 +0100

    add db models
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

---
+++ WebAppExpoGame/app/models/dbModels.js
@@ -0,0 +1,64 @@
+var mongoose = require( 'mongoose' );
+var IP_MONGODB = 'localhost'
+
+var modelIngredient = 'ingredient'
+var ingredientCollection = 'ingredients'
+
+var ingredient = new mongoose.Schema({
+	names: Array,
+	imageUrl: String,
+	category: mongoose.Schema.Types.ObjectId
+},{collection: ingredientCollection});
+
+mongoose.model( modelIngredient, ingredient);
+
+var modelMascot = 'mascot'
+var mascotCollection = 'mascots'
+
+var mascot = new mongoose.Schema({
+	category:Array,
+	latitude:Number,
+	longitude:Number,
+	modelUrl:String,
+	name:String
+
+},{collection:mascotCollection});
+
+mongoose.model( modelMascot , mascot);
+
+var modelDish = 'dish'
+var dishCollection = 'dishes'
+
+var dish = new mongoose.Schema({
+	names:Array,
+	nationality:String,
+	imageUrl:String,
+	descriptions:Array,
+	ingredients:Array,
+	zone:String
+},{collection:dishCollection});
+
+mongoose.model( modelDish , dish);
+
+var modelStatistic = 'statistic'
+var statisticCollection = 'statistics'
+
+var statistic = new mongoose.Schema({
+
+},{collection:statisticCollection});
+
+mongoose.model( modelStatistic , statistic);
+
+mongoose.connect( 'mongodb://'+IP_MONGODB+'/expogame' );
+
+exports.modelIngredient = modelIngredient
+exports.ingredients = mongoose.connection.collection(ingredientCollection)
+
+exports.modelMascot = modelMascot
+exports.mascots = mongoose.connection.collection(mascotCollection)
+
+exports.modelDish = modelDish
+exports.dishes = mongoose.connection.collection(dishCollection)
+
+exports.modelStatistic = modelStatistic
+exports.statistics = mongoose.connection.collection(statisticCollection)
