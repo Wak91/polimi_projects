37a6ba9c66dd0bd0f073bdc0955ecd30934347b1
degrigis <degrigis@gmail.com>
2015-01-09 16:04:01 +0100

    stub dishesprovider started
    Signed-off-by: degrigis <degrigis@gmail.com>

--- ExpoChef/app/src/main/java/it/polimi/expogame/database/MascotsTable.java
+++ ExpoChef/app/src/main/java/it/polimi/expogame/database/MascotsTable.java
@@ -10,4 +10,5 @@ public class MascotsTable {
     public static final String COLUMN_LATITUDE= "latitude";
     public static final String COLUMN_LONGITUDE = "longitude";
     public static final String COLUMN_IMAGE = "image";
+
 }
