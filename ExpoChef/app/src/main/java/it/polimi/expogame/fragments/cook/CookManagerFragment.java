7cd543dac17ebb84c092a6e94d576e7b13c3d363
Andrea Corna <andrea.corna.ac.91@gmail.com>
2015-01-23 10:40:17 +0100

    add arraylist ingredients to combine and sort method
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- ExpoChef/app/src/main/java/it/polimi/expogame/fragments/cook/CookManagerFragment.java
+++ ExpoChef/app/src/main/java/it/polimi/expogame/fragments/cook/CookManagerFragment.java
@@ -1,17 +1,16 @@
 package it.polimi.expogame.fragments.cook;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.TextView;
 
-import java.lang.annotation.Inherited;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import it.polimi.expogame.R;
 import it.polimi.expogame.support.Dish;
@@ -19,58 +18,30 @@ import it.polimi.expogame.support.ImageAdapter;
 import it.polimi.expogame.support.Ingredient;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link CookManagerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CookManagerFragment extends Fragment implements  CookFragment.OnDishCreatedListener, IngredientFragment.OnIngredientSelectedListener{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private ArrayList<Ingredient> ingredientsSelected;
     private GridView gridView;
     private ImageAdapter imageAdapter;
+    private ArrayList<String> ingredientsToCombine;
 
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CookManagerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CookManagerFragment newInstance(String param1, String param2) {
+    public static CookManagerFragment newInstance() {
         CookManagerFragment fragment = new CookManagerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     public CookManagerFragment() {
+
         ingredientsSelected = new ArrayList<Ingredient>();
+        ingredientsToCombine = new ArrayList<String>();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -85,10 +56,6 @@ public class CookManagerFragment extends Fragment implements  CookFragment.OnDis
         return currentView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-
-    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -127,6 +94,18 @@ public class CookManagerFragment extends Fragment implements  CookFragment.OnDis
         imageAdapter.notifyDataSetChanged();
         getView().invalidate();
 
+    }
+
+    
+
+    //method used to sort the arraylist of name of ingredients
+    private void reorderListIngredientsToCombine(){
+        Collections.sort(ingredientsToCombine, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.compareToIgnoreCase(s2);
+            }
+        });
     }
 
 }
