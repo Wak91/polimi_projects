8cfe40fa8972a7b725186145c1f4cb2f39dae531
Andrea Corna <andrea.corna.ac.91@gmail.com>
2015-02-03 13:48:56 +0100

    correct generation xml
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- WebAppExpoGame/app/libs/xmlGenerator.js
+++ WebAppExpoGame/app/libs/xmlGenerator.js
@@ -2,6 +2,7 @@ var builder = require('xmlbuilder');
 var fs = require('fs');
 var dir = require('path');
 var path = dir.join(__dirname, 'generated/xmls/')
+var S=require('string');
 
 
 
@@ -13,7 +14,8 @@ var writeXmlItalian = function(ingredientsList,dishesList,zonesList,mascotsList)
 	rootItalian.com("INGREDIENTS NAMES");
 	ingredientsList.forEach(function(ingredient){
 		var item = rootItalian.ele('string');
-		item.att("name",ingredient["name"].replace(" ", "_"));
+
+		item.att("name",S(ingredient["name"]).replaceAll(" ", "_"));
 		item.txt(ingredient["name"]);
 
 		 
@@ -22,30 +24,30 @@ var writeXmlItalian = function(ingredientsList,dishesList,zonesList,mascotsList)
 	rootItalian.com("ZONES NAMES");
 	zoneList.forEach(function(zone){
 		var item = rootItalian.ele('string');
-		item.att("name",zone["zone"].replace(" ", "_"));
+		item.att("name",S(zone["zone"]).replaceAll(" ", "_"));
 		item.txt(zone["zone"]);
 	});
 
 	rootItalian.com("DISHES NAMES - DESCRIPTIONS - CURIOSITY");
 	dishesList.forEach(function(dish){
 		var nationality = rootItalian.ele('string');
-		nationality.att("name",dish["nationality"].replace(" ", "_"));
+		nationality.att("name",S(dish["nationality"]).replaceAll(" ", "_"));
 		nationality.txt(dish["nationality"]);
 
 		var description = rootItalian.ele('string');
 		description.att("name",dish["name"]+"_descr");
-		description.txt(dish["description"]);
+		description.txt(S(dish["description"]).replaceAll("'","\'"));
 
 		var curiosity = rootItalian.ele('string');
 		curiosity.att("name",dish["name"]+"_curiosity");
-		curiosity.txt(dish["curiosity"]);
+		curiosity.txt(S(dish["curiosity"]).replaceAll("'","\'"));
 
 	});
 
 	rootItalian.com("MASCOTS CATEGORIES")
 	mascotsList.forEach(function(mascot){
 		var item = rootItalian.ele('string');
-		item.att("name",mascot["category"].replace(" ","_"))
+		item.att("name",S(mascot["category"]).replaceAll(" ","_"))
 		item.txt(mascot["category"]);
 	});
 
