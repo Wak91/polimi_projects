ee2b3e2e8de5095c9a3c10bd299be20b0ce7cd24
degrigis <degrigis@gmail.com>
2015-03-21 12:24:13 +0100

    added comments for PageCount
    Signed-off-by: degrigis <degrigis@gmail.com>

--- HadoopLogProcessing/src/hadoop/waxy/page/count/PageCount.java
+++ HadoopLogProcessing/src/hadoop/waxy/page/count/PageCount.java
@@ -14,6 +14,15 @@ import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+/*
+ * This mapreduce job will count the Waxy.org total pageviews per day in the entire time range.
+ * The final result on hdfs will be 
+ * 
+ * 1 May 2003 123762
+ * 2 May 2003 1239218
+ * [ ... ]
+ * 
+ * */
 public class PageCount  extends Configured implements Tool  {
 
 	@Override
@@ -29,32 +38,17 @@ Configuration conf = getConf();
         //Name of the job
         job.setJobName("viewcount");
 		
-        //Define the class of the key of the reduced tuples
         job.setOutputKeyClass(Text.class);
         
-        //Define the class of the value of the reduced tuples 
         job.setOutputValueClass(IntWritable.class);
 		
-        
-        
-        //Who is the mapper of this job?
         job.setMapperClass(PageCountMap.class);
         
-        //Who is the combiner of this job?
         job.setCombinerClass(PageCountReduce.class);
         
-        //Who is the reducer of this job?
         job.setReducerClass(PageCountReduce.class);
 		
-        /*
-         * The input format define how the input file is splitted and readed by Hadoop,
-         * in this case the TextInputFormat indicates that each line in the file is a record, and
-         the key is the byte offset of the line, and value is the content of the line */
         job.setInputFormat(TextInputFormat.class);
-        
-        /*This define how you are going to write on the HDFS, in this case 
-         * the tuple <V3,K3> is converted toString and separated by a \t 
-         * */
         job.setOutputFormat(TextOutputFormat.class);
 		
         //where the input will be
@@ -63,14 +57,8 @@ Configuration conf = getConf();
 		//where you want the output
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-        //----CONFIGURATION OF THE NEW JOB
-
-		
-		
 		JobClient.runJob(job);
-		
-		
-		
+	
 		return 0;
 	}
 	
--- HadoopLogProcessing/src/hadoop/waxy/page/count/PageCountMap.java
+++ HadoopLogProcessing/src/hadoop/waxy/page/count/PageCountMap.java
@@ -19,6 +19,10 @@ import org.apache.hadoop.mapred.Reporter;
  * If yes we are going to put in K2 the date of the request and in v2 'one'.
  * So the reducer are going to receive the 'one' of a specific date and can aggregate them.
  * 
+ * <(1 May 2003), 1>
+ * <(2 May 2003), 1>
+ * [ ... ]
+ * 
  * */
 public class PageCountMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -31,24 +35,23 @@ public class PageCountMap extends MapReduceBase implements Mapper<LongWritable, 
 		
 		String line = value.toString();
 		String date="";
-		
-		//TODO here let's perform the filter
-		
+				
 		StringTokenizer tokenizer = new StringTokenizer(line);
 		while (tokenizer.hasMoreTokens()) {
 			
 			String text="";
 			text = tokenizer.nextToken();
 			
+			//regex in order to match the date token
 			if(text.matches("\\[\\d{2}/\\w{3}/\\d{4}.*")){
 				
-				date = text.substring(1,12);		
+				date = text.substring(1,12);	//filter only what we need 	
 			}
 			
 			//it will be always a GET on the waxy.org page 
 			if(text.contains("GET")){
 				word.set(date);
-				output.collect(word, one);				
+				output.collect(word, one); //send to the reducers 				
 			}			
 		}
 		
